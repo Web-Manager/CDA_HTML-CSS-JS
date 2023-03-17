1727e3c1282f444ea51dcfbc2add6cc63925461f
Web Manager <manager-web@outlook.com>
2023-03-17 15:55:17 +0100

    TP_JS-Dom1 : Exercice Nombre Secret

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,13 +1,63 @@
+//***** Functions *****/
+/*
+ * Function to simplify the console display
+ * @param {type} str : string to display
+ * @returns {undefined}
+ */
+function display(str) {
+  console.log(str);
+}
+
+/*
+ * Function to know if a string is a integer
+ * @param {type} str : string to verify
+ * @returns {Boolean} : return true or false
+ */
+function valueIsInt(str) {
+  return Number.isInteger(parseInt(str.value));
+}
+
+/*
+ * Function : isNotEmpty - Verify if a string isn't empty
+ * @param {type} str : string controled
+ * @returns {Boolean} : return true or false
+ */
+function isNotEmpty(str) {
+  let result = false;
+
+  if (str.value !== "") {
+    result = true;
+  }
+
+  return result;
+}
+
+/*
+ * Obtain a random integer
+ * @param {type} min : the minimum value
+ * @param {type} max : the maximum value
+ * @returns {Number} : return the random integer
+ */
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+//*** End Functions
+
+const msgAlertNumber = "Merci de saisir un nombre";
+
+
 /* Exercice Conversion
- * 
+ *
  *  Ecrivez un programme pour permettre à l'utilisateur d'entrer un temps
  *  en seconde, et le convertir en minutes.
- *     
+ *
  *  La conversion doit s'afficher sur la page HTML dans la div avec l'id convert
- *  
+ *
  *  Vous devez afficher la conversion sous cette forme : xx secondes est égal à x minute
- *  
- *  Contraintes : 
+ *
+ *  Contraintes :
  *    Vous devez prendre en compte :
  *      - D'afficher seconde au singulier s'il n'y a qu'une seconde
  *      - D'afficher seconde au pluriel s'il y a plus d'une seconde
@@ -21,17 +71,23 @@ const tagConvert = document.querySelector("#convert");
 
 
 addButton.addEventListener('click', (event) => {
-  if (tagSeconds.value !== "") {
+  if (isNotEmpty(tagSeconds) && valueIsInt(tagSeconds)) {
     let nowDate = new Date();
+    let daysStr = " jour ";
     let hoursStr = " heures ";
     let minutesStr = " minutes ";
     let secondsStr = " secondes ";
-    let result = tagSeconds.value + " seconde(s) se converti en => ";
+    let resultConversion = tagSeconds.value + " seconde(s) se converti en => ";
 
     nowDate.setTime(parseInt(tagSeconds.value) * 1000);
+    let convertDays = nowDate.getDay();
     let convertHours = nowDate.getHours() - 1;
     let convertMinutes = nowDate.getMinutes();
     let convertSeconds = nowDate.getSeconds();
+
+    if (convertDays === 0 || convertHours === 1) {
+      hoursDays = " jour ";
+    }
 
     if (convertHours === 0 || convertHours === 1) {
       hoursStr = " heure ";
@@ -45,16 +101,81 @@ addButton.addEventListener('click', (event) => {
       secondsStr = " seconde";
     }
 
-    result += convertHours + hoursStr
+    resultConversion += convertDays + daysStr
+            + convertHours + hoursStr
             + convertMinutes + minutesStr
             + convertSeconds + secondsStr;
 
-    tagConvert.innerText = result;
+    tagConvert.innerText = resultConversion;
 
-    console.log(result);
+    console.log(resultConversion);
+  } else {
+    tagConvert.innerText = msgAlertNumber;
   }
   ;
 
   event.preventDefault();
 });
 
+/* Exercice : Nombre secret
+ *
+ * Développez le jeu pour découvrir le nombre secret.
+ * Cette fois-ci, vous devez laisser le joueur choisir le nombre.
+ *
+ * A chaque nombre choisi, vous devez afficher : Plus grand, plus petit,
+ * gagné ou perdu selon les cas.
+ *
+ * Le joueur n'a le droit qu'à 10 essai.
+ *
+ * Vous devez afficher et laisser choisir le joueur directement
+ * sur la page HTML
+ */
+const btnTest = document.querySelector('#btnTest');
+const tagNumber = document.querySelector("#number");
+const msgSecretNumber = document.querySelector("#msgSecretNumber");
+const tryLimit = 10;
+const minNumber = 1;
+const maxNumber = 50;
+const newP = document.createElement('p');
+
+let randomNumber = getRandomInt(1, 50);
+let tryNumber = 1;
+let msgResult = "";
+
+btnTest.addEventListener('click', (event) => {
+  if (isNotEmpty(tagNumber)
+          && valueIsInt(tagNumber)
+          && parseInt(tagNumber.value) >= minNumber
+          && parseInt(tagNumber.value) <= maxNumber)
+  {
+
+    let msgTry = `Essai ${tryNumber} : votre choix est le ${tagNumber.value}.....`;
+
+    if (tryNumber < tryLimit) {
+      if (parseInt(tagNumber.value) < randomNumber) {
+        msgResult += `${msgTry} echoué ! Le nombre à découvrir est plus grand<br>`;
+        
+      } else if (parseInt(tagNumber.value) > randomNumber) {
+        msgResult += `${msgTry} echoué ! Le nombre à découvrir est plus petit<br>`;
+      } else {
+        msgResult += `<p>${msgTry} BRAVO !!!! Vous avez gagné...</p>`;
+      }
+
+      tryNumber++;
+    } else {
+//      let newP = document.createElement("p");
+      
+      msgResult += `<p>${msgTry}</p>`;
+      msgResult += "<p>Oulala ! Vous n'avez pas trouvé<br>";
+      msgResult += `Le nombre était : ${randomNumber}</p>`;
+      msgResult += "Une autre tentative ? -> Rafraichissez la page";
+    }
+  } else {
+    msgSecretNumber.innerHTML = msgAlertNumber + " entre 1 et 50";
+  }
+  console.log(msgResult + " * " + randomNumber);
+
+  msgSecretNumber.innerHTML = msgResult;
+
+  event.preventDefault();
+});
