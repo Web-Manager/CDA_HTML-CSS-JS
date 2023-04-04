4e55d60d821df621dbe9bac20f3cf77ecf26cc0f
Web Manager <manager-web@outlook.com>
2023-04-04 19:13:16 +0200

    CDA - EX_Portfolio -> Install package Moka

---
+++ test/test.js
@@ -0,0 +1,16 @@
+/* 
+ * All rights reserverd to Gilbert CAPUTO-CERNI.
+ * This application is to use as that. Not modifications or partial code use are autorized.
+ * If you want informations contact me at : manager-web@outlook.com
+ * 
+ */
+var assert = require('assert');
+describe('Array', function () {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
+
+
